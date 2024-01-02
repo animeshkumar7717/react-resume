7bbd1627fb42da07c5bd777fb20c9ca36c146737
Animesh Kumar <animesh.kumar7717@gmail.com>
2024-01-02 23:10:41 +0530

    include navbar in every pages

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Navbar from './components/navbar/Navbar';
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <Navbar />
       <Pages />
     </BrowserRouter>
   );
