ac8575cf3bbb7bdd6682c42b8aa998a15aaabfa1
Animesh Kumar <animesh.kumar7717@gmail.com>
2024-01-02 23:06:54 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min.js';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/pages/Home';
-import Skills from './components/pages/skills/Skills';
-import Experiences from './components/pages/experiences/Experiences';
-import Educations from './components/pages/educations/Educations';
-import Portfolios from './components/pages/portfolios/Portfolios';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Pages from './pages';
 import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
+    <BrowserRouter>
+      {/* <Navbar /> */}
       <Pages />
-    </Router>
+    </BrowserRouter>
   );
 }
 
